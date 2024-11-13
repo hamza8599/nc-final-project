da54b39d128aea9560c7a56852e109f2e3ce4b87
hamza8599 <hamzashoukat787@gmail.com>
2024-11-13 15:14:30 +0000

    refoctored existing secrets method

--- DB/utils.py
+++
@@ -1,8 +0,0 @@
-# import pandas as pd
-# import pyarrow as pa
-# def format_to_parquet(data,conn):
-#     columns = [col["name"] for col in conn.columns]
-#     df=pd.DataFrame(data,columns=columns)
-#     table=pa.Table.from_pandas(df)
-#     return table
-    
