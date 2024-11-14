39f7f341b567766e0bd3427b11ba13c19f4b0a65
hamza8599 <hamzashoukat787@gmail.com>
2024-11-14 14:40:15 +0000

    refactored lambda function

--- data_extraction.py
+++ data_extraction.py
@@ -6,11 +6,10 @@ import pandas as pd
 import pyarrow as pa
 from pg8000.native import Connection
 import os
-#from dotenv import load_dotenv
 import json
 from botocore.exceptions import ClientError
 import logging
-#load_dotenv()
+
 
 def connect_db(secret_name):
     secrets_manager_client = boto3.client('secretsmanager')
@@ -27,6 +26,19 @@ def connect_db(secret_name):
 
 def close_db(conn):
     conn.close()
+
+def reset_secrets(sm_client,secret_name):
+    try:
+        response=sm_client.list_secrets(MaxResults=100)
+        for resp in response['SecretList']:
+            if secret_name==resp['Name']:
+                response=sm_client.put_secret_value(
+                SecretId=secret_name,
+                SecretString='2020-11-14 09:41:09.839000'
+                )
+
+    except ClientError as e:
+            print(e)
 
 def existing_secret(sm_client,secret_name):
     try:
@@ -75,6 +87,13 @@ def write_table_to_parquet_buffer(pyarrow_table):
     parquet_buffer.seek(0)
     return parquet_buffer
 
+def get_created_date(data,conn):
+     columns = [col["name"] for col in conn.columns]
+     df = pd.DataFrame(data, columns=columns)
+     created_at = df['created_at'].max()
+     return created_at
+
+
 
 
 logger = logging.getLogger()
@@ -82,6 +101,7 @@ logger.setLevel(logging.INFO)
 
 
 def lambda_handler(event, context):
+
     try:
         conn = connect_db('psql_creds')
         s3_client = boto3.client('s3')
@@ -92,40 +112,39 @@ def lambda_handler(event, context):
         
         for table in table_names:
             exist=existing_secret(secret_manager_client,table[0])
-            if exist:
+
+            if exist: 
                 response = secret_manager_client.get_secret_value(SecretId=table[0])
                 last_updated = response['SecretString']
                 
-            
                 last_updated_obj = datetime.strptime(last_updated, '%Y-%m-%d %H:%M:%S.%f')
-                
-                
+                   
                 last_updated_str = last_updated_obj.strftime('%Y-%m-%d %H:%M:%S.%f')
             
-                
                 query = f"SELECT * FROM {table[0]} WHERE last_updated > '{last_updated_str}';"
 
             else:
-                query = f"SELECT * FROM {table[0]}"
+                query = f"SELECT * FROM {table[0]};"
 
-        
             data = conn.run(query)
             # print(f"New Data : {data}")
-
+     
             if data:
                 print(f"Adding New Data in {table[0]}")
                 logger.info(f"Adding New Data in {table[0]}")
                 formatted_data = format_to_parquet(data, conn, table[0])
                 parquet_buffer = write_table_to_parquet_buffer(formatted_data)
+
                 timestamp = table[0]+" "+datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-                year=last_updated_obj.year
-                month=last_updated_obj.strftime('%B')
-                day=last_updated_obj.day
+                created_at=get_created_date(data,conn)
+                year=created_at.year
+                month=created_at.strftime('%B')
+                day=created_at.day
                 s3_key=f'{table[0]}/{year}/{month}/{day}/{timestamp}'
     
                 
                 try:
-                    s3_client.put_object(Bucket="team10-dimensional-transformers-ingestion-bucket", Key=s3_key, Body=parquet_buffer)
+                    s3_client.put_object(Bucket="dimensional-transformers-ingestion-bucket", Key=s3_key, Body=parquet_buffer)
                 except ClientError as e:
                     logger.info(f"expected Exception: {str(e)}")
                     print(e)
@@ -137,3 +156,10 @@ def lambda_handler(event, context):
     except Exception as e:
         logger.info(f"Unexpected Exception: {str(e)}")
 
+# lambda_handler("s","s")
+# resting secrets
+# conn = connect_db('psql_creds')
+# secret_manager_client = boto3.client('secretsmanager')
+# table_names = conn.run("SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE' AND table_name NOT LIKE '%prisma%';")
+# for table in table_names:
+#     reset_secrets(secret_manager_client,table[0])
