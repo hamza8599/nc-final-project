b474163067b11a093f9abc03e9d159b8ff720927
RohailZ <rz.dhothar@gmail.com>
2024-11-15 11:26:31 +0000

    stage 1 test 3 with ci cd

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,15 +3,15 @@ variable "region" {
 }
 
 variable "ingestion_bucket" {
-    default = "dimensional-transformers-ingestion-bucket"
+    default = "team-12-dimensional-transformers-ingestion-bucket"
 }
 
 variable "processed_bucket" {
-  default = "dimensional-transformers-process-bucket"
+  default = "team-12-dimensional-transformers-process-bucket"
 }
 
 variable "lambda_bucket" {
-    default = "dimensional-transformers-lambda-bucket"
+    default = "team-12-dimensional-transformers-lambda-bucket"
 }
 
 variable "lambda_ingestion" {
