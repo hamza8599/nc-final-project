8b37a162a537cd54a81cb1ed786d61a295f041e2
Laura Pugsley <lpugsley@hotmail.co.uk>
2024-11-18 16:48:56 +0000

    refactor processing code

--- dim_tables.py
+++ dim_tables.py
@@ -3,34 +3,50 @@ import boto3
 import awswrangler as wr
 from datetime import datetime
 
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# !!!!CHANGE BUCKET NAME!!!!!!
 
 s3 = boto3.client('s3')
+ingestion_bucket_name = ""
+process_bucket_name = ""
+table_name = ""
+"""get bucket name and object name from event"""
+# s3_bucket_name, s3_object_name = get_object_path(event["Records"])
+# logger.info(f"Bucket is {s3_bucket_name}")
+# logger.info(f"Object key is {s3_object_name}")
 
+"""Read Parquet from ingestion bucket"""
+df = wr.s3.read_parquet(f"s3://{ingestion_bucket_name}/{table_name}/*", dataset=True)
+
+"""checks for existing data processed bucket"""
+# try:
+#     existing_df = df
+#     combined = existing_df.merge(new_design_df, on='design_id')
+# except wr.exceptions.NoFilesFound:
+#     combined = new_design_df
+
+"""Write processed data to process bucket"""
+wr.s3.to_parquet(
+    df=df,
+    path=f's3://{process_bucket_name}/{table_name}/{table_name}.parquet'
+    # mode="overwrite",
+    # dataset=True
+)
 
 """Create dim_design"""
-design_df = wr.s3.read_parquet("s3://dorota-test-team10-dimensional-transformers-ingestion-bucket/design/*", dataset=True)
-new_design_df = design_df.drop(columns=['created_at', 'last_updated'])
-
+design_df = df.drop(columns=['created_at', 'last_updated'])
 
 """Create dim_currency"""
-currency_df = wr.s3.read_parquet("s3://dorota-test-team10-dimensional-transformers-ingestion-bucket/currency/*", dataset=True)
-new_currency_df = currency_df.drop(columns=['created_at', 'last_updated'])
+currency_df = df.drop(columns=['created_at', 'last_updated'])
 currency = {'GBP': 'Great British Pound', 'USD':'United States Dollar', 'EUR':'Euro'}
-new_currency_df['currency_name'] = new_currency_df['currency_code'].map(currency)
-
+currency_df['currency_name'] = currency_df['currency_code'].map(currency)
 
 """Create dim_staff"""
-staff_df = wr.s3.read_parquet("s3://dorota-test-team10-dimensional-transformers-ingestion-bucket/staff/*", dataset=True)
-dept_df = wr.s3.read_parquet("s3://dorota-test-team10-dimensional-transformers-ingestion-bucket/department/*", dataset=True)
+staff_df = wr.s3.read_parquet(f's3://{ingestion_bucket_name}/staff/*', dataset=True)
+dept_df = wr.s3.read_parquet(f's3://{ingestion_bucket_name}/department/*', dataset=True)
 dim_staff_df = pd.merge(staff_df, dept_df, on="department_id")[["staff_id", "first_name", "last_name", "department_name", "location", "email_address"]]
-# print(dim_staff_df)
-
 
 """Create dim_counterparty"""
-address_df = wr.s3.read_parquet("s3://dorota-test-team10-dimensional-transformers-ingestion-bucket/address/*", dataset=True)
-cp_df = wr.s3.read_parquet("s3://dorota-test-team10-dimensional-transformers-ingestion-bucket/counterparty/*", dataset=True)
+address_df = wr.s3.read_parquet(f's3://{ingestion_bucket_name}/address/*', dataset=True)
+cp_df = wr.s3.read_parquet(f's3://{ingestion_bucket_name}/counterparty/*', dataset=True)
 dim_counterparty_df = pd.merge(cp_df, address_df, left_on="legal_address_id", right_on="address_id", how="inner")[[
     "counterparty_id",
     "counterparty_legal_name",
@@ -50,16 +66,12 @@ dim_counterparty_df = pd.merge(cp_df, address_df, left_on="legal_address_id", ri
     "country": "counterparty_legal_country",
     "phone": "counterparty_legal_phone_number"
 })
-# print(dim_counterparty_df.to_string())
-
 
 """Create dim_location"""
-sales_order_df = wr.s3.read_parquet("s3://dorota-test-team10-dimensional-transformers-ingestion-bucket/sales_order/*", dataset=True)
+sales_order_df = wr.s3.read_parquet(f"s3://{ingestion_bucket_name}/sales_order/*", dataset=True)
 dim_location_df = pd.merge(sales_order_df, address_df, left_on="agreed_delivery_location_id", right_on="address_id", how="inner")[[
     "agreed_delivery_location_id", "address_line_1", "address_line_2", "district", "city", "postal_code", "country", "phone"
 ]].rename(columns={"agreed_delivery_location_id": "location_id"})
-# print(dim_location_df.to_string())
-
 
 """Create dim_date"""
 start_date = datetime(2020, 1, 1)
@@ -73,8 +85,6 @@ dim_date_df['day_of_week'] = dim_date_df['date_id'].dt.weekday
 dim_date_df['day_name'] = dim_date_df['date_id'].dt.day_name()
 dim_date_df['month_name'] = dim_date_df['date_id'].dt.month_name()
 dim_date_df['quarter'] = dim_date_df['date_id'].dt.quarter
-# print(dim_date_df)
-
 
 """Create fact_sales_order"""
 sales_order_df['created_date'] = pd.to_datetime(sales_order_df['created_at']).dt.date
@@ -82,18 +92,3 @@ sales_order_df['created_time'] = pd.to_datetime(sales_order_df['created_at']).dt
 sales_order_df['updated_date'] = pd.to_datetime(sales_order_df['last_updated']).dt.date
 sales_order_df['updated_time'] = pd.to_datetime(sales_order_df['last_updated']).dt.time
 fact_sales_order_df = sales_order_df.drop(columns=['created_at', 'last_updated'])
-
-# """checks for existing data processed bucket"""
-# try:
-#     existing_df = wr.s3.read_parquet("s3://dorota-test-team10-dimensional-transformers-process-bucket/design/dim_design", dataset=True)
-#     combined = existing_df.merge(new_design_df, on='design_id')
-# except wr.exceptions.NoFilesFound:
-#     combined = new_design_df
-
-"""Write processed data to process bucket"""
-wr.s3.to_parquet(
-    df=fact_sales_order_df,
-    path='s3://dorota-test-team10-dimensional-transformers-process-bucket/fact_sales_order/fact_sales_order.parquet'
-    # mode="overwrite",
-    # dataset=True
-)
