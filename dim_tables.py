e6bd215bc0ef4a21f492c5d84a9532142d797141
willr196 <willrobb11@gmail.com>
2024-11-19 10:19:25 +0000

    fixed some errors

--- dim_tables.py
+++ dim_tables.py
@@ -3,35 +3,57 @@ import boto3
 import awswrangler as wr
 from datetime import datetime
 
-
-s3 = boto3.client('s3')
-ingestion_bucket_name = ""
-process_bucket_name = ""
-table_name = ""
+other_data = "table_name = ""
 """get bucket name and object name from event"""
 # s3_bucket_name, s3_object_name = get_object_path(event["Records"])
 # logger.info(f"Bucket is {s3_bucket_name}")
-# logger.info(f"Object key is {s3_object_name}")
+# logger.info(f"Object key is {s3_object_name}")"
 
 
-"""Create dim_design"""
-design_df = df.drop(columns=['created_at', 'last_updated'])
+s3 = boto3.client('s3')
+ingested_bucket_name = ""
+processed_bucket_name = ""
+to_not_delete = "s3://dorota-test-team10-dimensional-transformers-process-bucket/fact_sales_order/fact_sales_order.parquet"
 
-"""Create dim_currency"""
-currency_df = df.drop(columns=['created_at', 'last_updated'])
-currency = {'GBP': 'Great British Pound', 'USD':'United States Dollar', 'EUR':'Euro'}
-currency_df['currency_name'] = currency_df['currency_code'].map(currency)
 
-"""Create dim_staff"""
-staff_df = wr.s3.read_parquet(f's3://{ingestion_bucket_name}/staff/*', dataset=True)
-dept_df = wr.s3.read_parquet(f's3://{ingestion_bucket_name}/department/*', dataset=True)
-dim_staff_df = pd.merge(staff_df, dept_df, on="department_id")[["staff_id", "first_name", "last_name", "department_name", "location", "email_address"]]
+#reads the specific bucket & key from the body"
+def read_parquet_files(bucket, key):
+    object = s3.get_object(bucket, key)
+    return pd.read_parquet(object["body"])
 
-"""Create dim_counterparty"""
-address_df = wr.s3.read_parquet(f's3://{ingestion_bucket_name}/address/*', dataset=True)
-cp_df = wr.s3.read_parquet(f's3://{ingestion_bucket_name}/counterparty/*', dataset=True)
-dim_counterparty_df = pd.merge(cp_df, address_df, left_on="legal_address_id", right_on="address_id", how="inner")[[
-    "counterparty_id",
+def writing_to_parquet(df, bucket, key):
+    wr.s3.to_parquet(
+    df=df,
+    path=f's3://{bucket}/{key}'
+    # mode="overwrite",
+    # dataset=True
+)
+def create_dim_design():
+    design_df = read_parquet_files(f"s3://{ingested_bucket_name}/design/*", dataset=True)
+    design_df = design_df.drop(columns=['created_at', 'last_updated'])
+    return design_df[["design_id", "design_name", "file_location", "filename"]]
+
+def create_dim_currency():
+    currency_df = read_parquet_files(f's3://{ingested_bucket_name}/currency/*', dataset=True)
+    currency = {'GBP': 'Great British Pound', 'USD':'United States Dollar', 'EUR':'Euro'}
+    currency_df = currency_df.drop(columns=["created_at", "last_updated"])
+    currency_df["currency_name"] = currency_df["currency_code"].map(currency)
+    return currency_df[["currency_id", "currency_code", "currency_name"]]
+
+
+def create_dim_staff():
+    staff_df = wr.s3.read_parquet(f"s3://{ingested_bucket_name}/staff/*", dataset=True)
+    department_df = wr.s3.read_parquet(f's3://{ingested_bucket_name}/deparment/*', dateset=True)
+    departmental_staff = pd.merge(staff_df, department_df, on="department_id")
+    return departmental_staff[["staff_id", "first_name", "last_name", "department_name", "location", "email_address"]]
+
+
+
+def create_dim_counterparty():
+    address_df = wr.s3.read_parquet(f's3://{ingested_bucket_name}/address/*', dataset=True)
+    cp_df = wr.s3.read_parquet(f's3://{ingested_bucket_name}/cp/*', dataset=True)
+    dim_counterparty_df = pd.merge(cp_df, address_df, left_on="legal_address_id", right_on="address_id", how="inner")
+    return dim_counterparty_df[["counterparty_id",
     "counterparty_legal_name",
     "address_line_1",
     "address_line_2",
@@ -50,28 +72,65 @@ dim_counterparty_df = pd.merge(cp_df, address_df, left_on="legal_address_id", ri
     "phone": "counterparty_legal_phone_number"
 })
 
-"""Create dim_location"""
-sales_order_df = wr.s3.read_parquet(f"s3://{ingestion_bucket_name}/sales_order/*", dataset=True)
-dim_location_df = pd.merge(sales_order_df, address_df, left_on="agreed_delivery_location_id", right_on="address_id", how="inner")[[
-    "agreed_delivery_location_id", "address_line_1", "address_line_2", "district", "city", "postal_code", "country", "phone"
-]].rename(columns={"agreed_delivery_location_id": "location_id"})
 
-"""Create dim_date"""
-start_date = datetime(2020, 1, 1)
-end_date = datetime.today()
-date_range = pd.date_range(start=start_date, end=end_date, freq='D')
-dim_date_df = pd.DataFrame(date_range, columns=['date_id'])
-dim_date_df['year'] = dim_date_df['date_id'].dt.year
-dim_date_df['month'] = dim_date_df['date_id'].dt.month
-dim_date_df['day'] = dim_date_df['date_id'].dt.day
-dim_date_df['day_of_week'] = dim_date_df['date_id'].dt.weekday
-dim_date_df['day_name'] = dim_date_df['date_id'].dt.day_name()
-dim_date_df['month_name'] = dim_date_df['date_id'].dt.month_name()
-dim_date_df['quarter'] = dim_date_df['date_id'].dt.quarter
+def create_dim_location():
+    sales_order_df = wr.s3.read_parquet(f's3://{ingested_bucket_name}/location/*', dataset=True)
+    address_df = wr.s3.read_parquet(f's3://{ingested_bucket_name}/address/*', dataset=True)
+    dims_location_df = pd.merge(sales_order_df, address_df, left_on="agreed_delivery_location_id", right_on="address_id", how="inner")
+    return dims_location_df[["agreed_delivery_location_id", "address_line_1", "address_line_2", "district", "city", "postal_code", "country", "phone"]].rename(columns={"agreed_delivery_location_id": "location_id"})
+
+
+
+def create_dim_date():
+    start_date = datetime(2020, 1, 1)
+    end_date=datetime.today()
+    date_range = pd.date_range(start=start_date, end = end_date, freq='D')
+    dim_date_df = pd.DataFrame(date_range, columns=['date_id'])
+    dim_date_df['year'] = dim_date_df['date_id'].dt.year
+    dim_date_df['month'] = dim_date_df['date_id'].dt.month
+    dim_date_df['day'] = dim_date_df['date_id'].dt.day
+    dim_date_df['day_of_week'] = dim_date_df['date_id'].dt.weekday
+    dim_date_df['day_name'] = dim_date_df['date_id'].dt.day_name()
+    dim_date_df['month_name'] = dim_date_df['date_id'].dt.month_name()
+    dim_date_df['quarter'] = dim_date_df['date_id'].dt.quarter
+
+
+
+def create_facts_table():
+    sales_order_df = wr.s3.read_parquet(f"s3://{ingested_bucket_name}/sales_order/*", dataset=True)
+    sales_order_df["created_date"] = pd.to_datetime(sales_order_df["created_at"]).dt.date
+    sales_order_df["created_time"] = pd.to_datetime(sales_order_df["created_at"]).dt.time
+    sales_order_df["updated_date"] = pd.to_datetime(sales_order_df["last_updated"]).dt.date
+    sales_order_df["updated_time"] = pd.to_datetime(sales_order_df["last_updated"]).dt.time
+    return sales_order_df.drop(columns=["created_at", "last_updated"])
+
+
+if __name__ == "__main__":
+    dim_design = create_dim_design()
+    dim_currency = create_dim_currency()
+    dim_staff = create_dim_staff()
+    dim_counterparty = create_dim_counterparty()
+    dim_location = create_dim_location()
+    dim_date = create_dim_date()
+
+
+    fact_sales_order = create_facts_table()
+
+
+    writing_to_parquet(dim_design, processed_bucket_name, "dim_design/dim_design.parquet")
+    writing_to_parquet(dim_currency, processed_bucket_name, "dim_currency/dim_currency.parquet")
+    writing_to_parquet(dim_staff, processed_bucket_name, "dim_staff/dim_staff.parquet")
+    writing_to_parquet(dim_counterparty, processed_bucket_name, "dim_counterparty/dim_counterparty.parquet")
+    writing_to_parquet(dim_location, processed_bucket_name, "dim_location/dim_location.parquet")
+    writing_to_parquet(dim_date, processed_bucket_name, "dim_date/dim_date.parquet")
+    writing_to_parquet(fact_sales_order, processed_bucket_name, "fact_sales_order/fact_sales_order.parquet")
+
 
 """Create fact_sales_order"""
-sales_order_df['created_date'] = pd.to_datetime(sales_order_df['created_at']).dt.date
-sales_order_df['created_time'] = pd.to_datetime(sales_order_df['created_at']).dt.time
-sales_order_df['updated_date'] = pd.to_datetime(sales_order_df['last_updated']).dt.date
-sales_order_df['updated_time'] = pd.to_datetime(sales_order_df['last_updated']).dt.time
-fact_sales_order_df = sales_order_df.drop(columns=['created_at', 'last_updated'])
+
+# """checks for existing data processed bucket"""
+# try:
+#     existing_df = wr.s3.read_parquet("s3://dorota-test-team10-dimensional-transformers-process-bucket/design/dim_design", dataset=True)
+#     combined = existing_df.merge(new_design_df, on='design_id')
+# except wr.exceptions.NoFilesFound:
+#     combined = new_design_df
