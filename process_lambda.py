9cc3d995e1e0df899385ea0d3e1c8d14fcf1a44a
hamza8599 <hamzashoukat787@gmail.com>
2024-11-21 14:01:25 +0000

    write util func for loading data

--- process_lambda.py
+++ process_lambda.py
@@ -1,3 +1,3 @@
 import pandas as pd
-df = pd.read_parquet("/Users/hamzashoukat/Desktop/new-northcoders/nc-final-project/date.parquet")
+df = pd.read_parquet("/Users/hamzashoukat/Desktop/new-northcoders/nc-final-project/date (1).parquet")
 print(df.to_string())
--- sandbox.py
+++
@@ -1,8 +0,0 @@
-s3_object_name = "sales/12/15/10/sales-12350"
-tablename = s3_object_name.split('/', 5)[0]
-
-print(tablename)
-import awswrangler as wr
-import pandas as pd
-df = pd.read_parquet("d9dda52ec67f4ef7bd6e8b2a30171575.snappy.parquet")
-print(df.to_string())
